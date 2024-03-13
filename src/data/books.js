d0e10276b70b011badd68f6337a2f5ac52fda45c
davidingles <davidingleslopez@gmail.com>
2024-03-13 18:51:00 +0100

    david primer commit

---
+++ src/data/books.js
@@ -0,0 +1,291 @@
+export const books = [
+
+	{
+		id: '1P0221',
+		title: 'Bandeja con Cotas',
+		img: '/catalogo/1P0221.png',
+		opinion: 'Bandeja automontable acotada.',
+		author: 'David Inglés',
+		categoria: 'Bandejas',
+		tamaño: 3,
+		escala: 1,
+		posicion: .3,
+		glbSource: '/gltf/1P0221.glb',
+		three: '1P0221',
+		video: '/videos/montajes/v1P0221.webm',
+		scaleVideo: 1.5
+
+	},
+	{
+		id: 'embargos',
+		title: 'Bandeja con Cotas',
+		img: '/catalogo/render_embargos.png',
+		opinion: 'Bandeja automontable acotada.',
+		author: 'David Inglés',
+		categoria: 'expositor',
+		tamaño: 3,
+		escala: .25,
+		posicion: -.5,
+		glbSource: '/glb/embargos.glb',
+		three: 'embargos',
+		video: '/videos/montajes/video_embargo.webm',
+		scaleVideo: 1.5
+
+	},
+	{
+		id: 'v4G02870001_cazoletas',
+		title: 'Bandeja con Cotas',
+		img: '/catalogo/RenderEstucheCazoletas.png',
+		opinion: 'Bandeja automontable acotada.',
+		author: 'David Inglés',
+		categoria: 'Bandejas',
+		tamaño: 3,
+		escala: 1,
+		posicion: .3,
+		glbSource: '/gltf/4G0288_cazoletas.glb',
+		three: '1P0221',
+		video: '/videos/montajes/v4G02870001_cazoletas.webm',
+		scaleVideo: 1.5
+
+	},
+	{
+		id: 'cajaB1',
+		title: 'cajaB1',
+		img: '/catalogo/cajaB1.png',
+		opinion: 'Bandeja automontable acotada.',
+		author: 'David Inglés',
+		categoria: 'Cajas',
+		tamaño: 3,
+		escala: .8,
+		posicion: .3,
+		glbSource: '/gltf/cajaB1.glb',
+		three: '1P0221',
+		video: '/videos/montajes/caja.webm',
+		scaleVideo: 1.5
+
+	},
+	{
+		id: 'ExpositorDM',
+		title: 'PLV y Expositores',
+		img: '/catalogo/3D EXPOSITOR DM.png',
+		opinion: '"Estuche practo para el transporte del usuario final',
+		author: 'Vicente Latorre',
+		three: 'ExpositorDM',
+		categoria: 'expositor'
+
+	},
+	{
+		id: 'troquel1g0011',
+		title: 'Referencia: troquel1g0011',
+		img: '/catalogo/troquel1g0011.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'troquel1g0011',
+		glbSource: '/gltf/troquel1g0011.glb',
+		video: '/videos/montajes/v__1g0011.webm',
+		categoria: 'Bandejas',
+		tamaño: 1,
+		escala: 1,
+		posicion: .3
+	},
+	{
+		id: 'troquel1g0198',
+		title: 'Referencia: troquel1g0198',
+		img: '/catalogo/img1G0198.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'Troquel1g0198',
+		video: '/videos/montajes/video1g0198.mp4',
+		categoria: 'Bandejas',
+		tamaño: 1,
+		escala: .7,
+		posicion: -.3,
+		glbSource: '/gltf/Troquel1G0198.gltf'
+	},
+	{
+		id: 'troquel1g0102',
+		title: 'Referencia: troquel1g0102',
+		img: '/catalogo/troquel1g0102.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'Troquel1g0102',
+		categoria: 'Bandejas'
+	},
+	{
+		id: '1L0024',
+		troquel: '1L0024',
+		title: 'Referencia: BASTBM01',
+		img: '/catalogo/troquel1L0024.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: '1L0024',
+		categoria: 'Bandejas',
+		posicion: .3,
+		escala: 1,
+		video: '/videos/montajes/v_1L0024.webm',
+		tamaño: 1,
+		glbSource: '/gltf/1L0024.glb'
+	},
+	{
+		id: 'troquel1P0572',
+		title: 'Referencia: troquel1P0572',
+		img: '/catalogo/1P0572.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'Troquel1P0572',
+		categoria: 'Bandejas'
+	},
+	{
+		id: 'troquelComedero',
+		title: 'Referencia: troquelComedero',
+		img: '/catalogo/troquelComedero.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'TroquelComedero',
+		categoria: 'Bandejas'
+	},
+	{
+		id: 'troquelMesaFeria',
+		title: 'Referencia: troquelMesaFeria',
+		img: '/catalogo/troquelMesaFeria.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'TroquelMesaFeria',
+		categoria: 'expositor'
+	},
+	{
+		id: 'troquelMuebleMadera',
+		title: 'Referencia: troquelMuebleMadera',
+		img: '/catalogo/troquelMuebleMadera.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'TroquelMuebleMadera',
+		categoria: 'expositor'
+	},
+	{
+		id: 'troquelMando',
+		title: 'Referencia: troquelMando',
+		img: '/catalogo/troquelMando.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'TroquelMando',
+		categoria: 'Bandejas'
+	},
+	{
+		id: 'troquelMaleta',
+		title: 'Referencia: troquelMaleta',
+		img: '/catalogo/troquelMaleta.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'TroquelMaleta',
+		categoria: 'Bandejas'
+	},
+	{
+		id: 'troquelCajaB1',
+		title: 'Referencia: troquelCajaB1',
+		img: '/catalogo/troquelCajaB1.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'TroquelCajaB1',
+		categoria: 'Bandejas'
+	},
+	{
+		id: 'troquelCaballete',
+		title: 'Referencia: troquelCaballete',
+		img: '/catalogo/troquelCaballete.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'caballete-03',
+		categoria: 'expositor',
+		video: '/videos/montajes/videoCaballete.webm',
+		escala: .3,
+		posicion: .3,
+		tamaño: 1,
+		glbSource: '/gltf/caballete-03.glb'
+	},
+	{
+		id: 'troquel4e0066',
+		title: 'Referencia: troquel4e0066',
+		img: '/catalogo/Render4e0066-BIS.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'Troquel4e0066',
+		video: '/videos/montajes/video4e0066.webm',
+		categoria: 'Bandejas',
+		escala: .9,
+		posicion: -.4,
+		tamaño: 1,
+		glbSource: '/gltf/Troquel4e0066.gltf'
+	},
+
+	{
+		id: 'troquel1p0391',
+		title: 'Referencia: troquel1p0391',
+		img: '/catalogo/troquel1p0391.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: '1p0391',
+		video: '/videos/montajes/videoCajaConBotes.webm',
+		posicion: .3,
+		categoria: 'Cajas',
+		escala: 1.3,
+		tamaño: 1,
+		glbSource: '/gltf/1p0391.glb'
+	},
+	{
+		id: 'arbol',
+		title: 'Referencia: arbol',
+		img: '/catalogo/RenderArbol.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'DecoracionArbol',
+		video: '/videos/montajes/videoArbolito.webm',
+		categoria: 'expositor',
+		escala: .3,
+		posicion: -.5,
+		tamaño: 1,
+		glbSource: '/gltf/arbol y ciervo.gltf'
+	},
+	{
+		id: 'jamonero',
+		title: 'Referencia: jamoneroMacondo',
+		img: '/catalogo/JamoneroSinImpresion.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'jamoneroMacondo2',
+		video: '/videos/montajes/videoJamonero.webm',
+		categoria: 'Bandejas',
+		escala: .8,
+		posicion: .3
+
+	},
+	{
+		id: 'dianaTiro',
+		title: 'DIANA DE TIRO',
+		img: '/catalogo/AMI UNO2.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'DIANA',
+		// video: '/videos/montajes/',
+		categoria: 'expositor',
+		escala: .4,
+		glbSource: '/gltf/ami2.glb',
+		posicion: -.5,
+		tamaño: 1
+	},
+	{
+		id: 'donaciones',
+		title: 'donaciones',
+		img: '/catalogo/donaciones.png',
+		opinion: 'Estuche remontable con cierre de tapa y perforado para unir a otros estuches',
+		author: 'Vicente Latorre',
+		three: 'donaciones',
+		// video: '/videos/montajes/',
+		categoria: 'expositor',
+		escala: .3,
+		posicion: -.8,
+		tamaño: 1
+	}
+]
+
+// prueba
---
+++ src/data/libreto2.js
@@ -0,0 +1,47 @@
+export const libreto2 = [
+
+	{
+		tamaño: 1,
+		escala: .9,
+		posicion: 0.1,
+		glbSource: '/glb/JAMONERO9.glb',
+		velocidadRotacion: 6,
+
+	},
+	{
+		tamaño: 1,
+		escala: 1.5,
+		posicion: .0,
+		glbSource: '/glb/ROANO.glb',
+		velocidadRotacion: 6,
+
+	},
+	{
+		tamaño: 1,
+		escala: .5,
+		posicion: .0,
+		glbSource: '/glb/CANARA.glb',
+		velocidadRotacion: 6,
+	},
+	{
+		tamaño: 1,
+		escala: 1.2,
+		posicion: -.1,
+		glbSource: '/glb/PROSUR.glb',
+		velocidadRotacion: 6,
+	},
+	{
+		tamaño: 1,
+		escala: 1,
+		posicion: -.1,
+		glbSource: '/glb/LONCHEMUR.glb',
+		velocidadRotacion: 6,
+	},
+	{
+		tamaño: 1,
+		escala: 1,
+		posicion: .1,
+		glbSource: '/glb/LONCHEMUR1.glb',
+		velocidadRotacion: 6,
+	}
+]
